299d8a46c1e83f9ea1a4c00033ffb8efcc273ce3
felicienveldema <feli_ve@live.nl>
2018-05-24 14:47:50 +0200

    file

--- Assignment2/GBMfile.py
+++ Assignment2/GBMfile.py
@@ -10,9 +10,8 @@ import time
 import pickle
 from sklearn import ensemble
 
-def loadtraining():
+def loadtraining(path):
     #LOAD The data
-    path = 'Data Mining VU data/training_set_VU_DM_2014.csv'
     df = pd.read_csv(path, sep=',')
 
     Yclick = df['click_bool']
@@ -26,7 +25,7 @@ def loadtraining():
 
 def loadtest():
     #LOAD The data
-    path2 = 'Data Mining VU data/test_set_VU_DM_2014.csv'
+    path2 = 'Data Mining VU data/test.csv'
     dftest = pd.read_csv(path2, sep=',')
 
     #dumb clean NA features
@@ -38,43 +37,42 @@ def loadtest():
 
 
 def main():
-    createGBM = False
-    fitGBM = False
+    createGBM = True
+    fitGBM = True
 
     if(createGBM):
         print('create GBM')
-        GBM = sklearn.ensemble.GradientBoostingClassifier(loss='deviance', learning_rate=0.1,
-                                                    n_estimators=100, subsample=1.0, criterion='friedman_mse',
+        GBM = sklearn.ensemble.GradientBoostingClassifier(loss='deviance', learning_rate=0.15,
+                                                    n_estimators=400, subsample=1.0, criterion='friedman_mse',
                                                     min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
                                                     max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None,
                                                     init=None, random_state=None, max_features=None, verbose=1, max_leaf_nodes=None,
                                                     warm_start=False, presort='auto')
-        pickle.dump(GBM, open('clean_model.pkl',"wb"))
+        pickle.dump(GBM, open('clean_model1miltrainNoPosNoGros.pkl',"wb"))
     else:
-        GBM = pickle.load(open('clean_model.pkl', 'rb'))
+        GBM = pickle.load(open('clean_model1miltrainNoPosNoGros.pkl', 'rb'))
 
 
     print('load training')
-    [X, Yclick, Ybook] = loadtraining()
-    Xtrain = X[:100000]
+    [X, Yclick, Ybook] = loadtraining('Data Mining VU data/training_set_VU_DM_2014.csv')
+    Xtrain = X[:2000000]
 
 
-    Yclick = Yclick + Ybook
+    Ytrain = Yclick + Ybook
 
     print('fit')
     start = time.time()
     if(fitGBM):
-        GBM.fit(Xtrain, Yclick[:100000])
-        pickle.dump(GBM, open('fitted_model.pkl', 'wb'))
+        GBM.fit(Xtrain, Ytrain[:2000000])
+        pickle.dump(GBM, open('fitted_model1miltrainNoPosNoGros.pkl', 'wb'))
     else:
-        GBM = pickle.load(open('fitted_model.pkl', 'rb'))
+        GBM = pickle.load(open('fitted_model1miltrainNoPosNoGros.pkl', 'rb'))
     end = time.time()
     print(end - start)
 
     print('load test')
     start = time.time()
-    #Xtest = loadtest()
-    Xtest = X[100000:190000]
+    # [Xtest, Yclicktest, Ybooktest] = loadtraining('Data Mining VU data/train.csv')
     end = time.time()
     print(end - start)
 
@@ -84,12 +82,13 @@ def main():
     # end = time.time()
     # print(end - start)
 
-    print(Yclick[:4])
-    print(GBM.score(Xtest, Yclick[100000:190000]))
+    Ytest = Ytrain[2000000:3000000]
+    Xtest = X[2000000:3000000]
+    print(GBM.score(Xtest, Ytest))
 
     Y = GBM.predict(Xtest)
     print(sum(Y))
-
+    print(sum(Ytest))
 
 if __name__ == '__main__':
     main()
